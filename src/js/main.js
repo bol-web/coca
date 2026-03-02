812f49aaf3d7f8c756c9e5a153f4ec5b8a633626
TestName <bolotskiu2006@gmail.com>
2026-03-02 21:08:34 +0700

    Added format script

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 import '../assets/scss/main.scss';
 
 // components
-import {useTheme} from './components/theme.js';
-import {useBurger} from './components/burger.js';
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
 
 useTheme();
 useBurger();
