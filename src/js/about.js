2eca3304245ac4a03d6f468f6f2e591489c24fbd
TestName <bolotskiu2006@gmail.com>
2026-03-12 20:37:10 +0700

    Created brand philosophy section in About page

--- src/js/about.js
+++ src/js/about.js
@@ -3,6 +3,8 @@ import '/scss/about.scss';
 // components
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
+import { useBrandPhilosophySlider } from './components/about/slider.js';
 
 useTheme();
 useBurger();
+useBrandPhilosophySlider();
---
+++ src/js/components/about/slider.js
@@ -0,0 +1,16 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+
+export const useBrandPhilosophySlider = () => {
+  new Swiper('.brand-philosophy__slider', {
+    spaceBetween: 32,
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    breakpoints: {
+      1201: {
+        centeredSlides: false,
+      },
+    },
+  });
+};
